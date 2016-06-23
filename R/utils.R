09c62bfdc16ce43ddce18365b12aaa1b7f8de541
Jean-Philippe Fortin <jpfortin@blk145.wireless-pennnet.upenn.edu>
2016-06-23 13:39:31 -0400

    Added feeling

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,8 @@
-getColorFrancis <- function(){
-    "firebrick"
+getColorFrancis <- function(shade=c("happy", "angry")){
+    shade <- match.arg(shade)
+    if (shade=="happy"){
+        return("firebrick")
+    } else {
+        return("firebrick4")
+    }
 }
